2dd0bb30df7d22aa81042fa46ed3a46d404d1b80
Cadu <caduspereira.2004@gmail.com>
2025-03-03 22:53:57 -0300

    Teste

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+  public static void main(String[] args){
+
+      int x = 4;
+
+      if (x == 10) {
+          System.out.println("é 10!!");
+
+      }
+      else {
+          System.out.print("não é 10 e sim ");
+          System.out.print(x);
+
+      }
+
+  }
+
+
+}
+
